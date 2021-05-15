6c83453cbecb9ce104a7d0728abd1a9e2801c409
David <davidstankoviic@gmail.com>
2021-05-15 23:58:31 +0200

    Add: Loading Spinner

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+
+import Loader from '../components/Loader';
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'dev-david-s' }
-      }}><a>David's Profile</a></Link>
+      <Loader show />
     </div>
   )
 }
