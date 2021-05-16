af906d6838b41f2c2d52da3a0424611d9a6e48f7
David <davidstankoviic@gmail.com>
2021-05-16 16:11:39 +0200

    Add: User Authentication, Username setting and validation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
 
 export default function Navbar() {
-    const user = true;
-    const username = true;
+
+    const { user, username } = useContext(UserContext);
 
     return (
         <nav className="navbar">
@@ -16,13 +18,16 @@ export default function Navbar() {
                 {username && (
                     <>
                         <li className="push-left">
+                            <button>Sign Out</button>
+                        </li>
+                        <li>
                             <Link href="/admin">
                                 <button className="btn-blue">Write Posts</button>
                             </Link>
                         </li>
                         <li>
                             <Link href={`/${username}`}>
-                                <img src={user?.photoURL} />
+                                <img src={user?.photoURL || '/hacker.png'} />
                             </Link>
                         </li>
                     </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import { Toaster } from 'react-hot-toast'
 import Navbar from '../components/Navbar'
+import { UserContext } from '../lib/context'
 import '../styles/globals.css'
 
+import { auth, firestore } from '../lib/firebase';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useUserData } from '../lib/hooks';
+
 function MyApp({ Component, pageProps }) {
+
+  const userData = useUserData();
+
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   )
 }
 
